41982c72730c42587d136ce8be3c497cc62a868d
Thiago da Silva Lima <thiagodasilvalima@MacBook-Air-de-Thiago.local>
2021-04-01 17:40:30 -0300

    fixing_bugs

--- src/app/user/components/sign-up/sign-up.component.ts
+++ src/app/user/components/sign-up/sign-up.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {UserService} from '../../services/user.service';
-import {User} from '../../models/user';
+import {User} from '../../models/User';
 
 @Component({
   selector: 'app-sign-up',
